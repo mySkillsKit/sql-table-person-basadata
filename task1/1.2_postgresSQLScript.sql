d85a8ccb19c2d4f6ec05c2f7a6c4a4ec195c7ab5
Anatolii Vasilev <avas2012@gmail.com>
2022-09-09 20:00:31 +0300

    resolved task1

---
+++ task1/1.2_postgresSQLScript.sql
@@ -0,0 +1,11 @@
+/*
+ Напишите скрипт, который будет искать в таблице
+ PERSONS поля name и surname пользователей,
+ которые проживают в MOSCOW.
+ */
+
+SELECT name,
+       surname
+FROM persons
+WHERE city_of_living LIKE 'MOSCOW';
+--WHERE city_of_living = 'MOSCOW';
